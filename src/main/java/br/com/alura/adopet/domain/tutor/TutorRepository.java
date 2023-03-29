051c76af9d1edb58543f97c5c538269f5e457626
GiovanniKF <giiovanni.kf@gmail.com>
2023-03-29 10:20:58 -0300

    Revert "CRUD de Tutores em API (Semana 1)"
    This reverts commit 9ee0312175df5c309ec11a3e4d2d9aa38bc88db5.

--- src/main/java/br/com/alura/adopet/domain/tutor/TutorRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.alura.adopet.domain.tutor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TutorRepository extends JpaRepository<Tutor, Long> {
-}
